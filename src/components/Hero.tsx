cb18dbd1202565e2002ca43fc8a580337248fd0b
¨diegomorcillod¨ <diegomorcillod@gmail.com>
2026-01-04 15:20:50 +0100

    Cambiar fuente a Allura

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero: React.FC = () => {
   return (
     <section className="text-center">
       {/* ¡NOS CASAMOS! - Grande y con efectos */}
-      <h2 className="text-3xl sm:text-4xl md:text-5xl font-great-vibes text-canva-green mb-8 tracking-wider uppercase animate-pulse">
+      <h2 className="text-3xl sm:text-4xl md:text-5xl font-allura text-canva-green mb-8 tracking-wider uppercase animate-pulse">
         ¡NOS CASAMOS!
       </h2>
       
